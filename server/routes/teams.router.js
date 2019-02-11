dc2640e2e8e8ee50028c471b0b1645397db9edb6
Kye Berkeland <kyeberkeland@yahoo.com>
2019-02-11 13:08:04 -0600

    Create server get route for team data

---
+++ server/routes/teams.router.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const axios = require('axios');
+
+// constant variables for use in API url setup
+const BASE_URL = 'https://api.sportradar.us/soccer-xt3/eu/en/';
+const API_KEY = process.env.API_KEY;
+
+
+// Tournaments info
+// https://api.sportradar.us/soccer-xt3/eu/en/tournaments.json?api_key=j9xerbvc24veacrq3hpby6dk
+
+// Specific tournament info
+// https://api.sportradar.us/soccer-xt3/eu/en/tournaments/sr:tournament:17/info.json?api_key=j9xerbvc24veacrq3hpby6dk
+
+// Team info
+// https://api.sportradar.us/soccer-xt3/eu/en/tournaments/sr:tournament:17/teams/sr:competitor:48/statistics.json?api_key=j9xerbvc24veacrq3hpby6dk
+
+// GET route to select team data from Sportradar.us API after login
+router.get('/', rejectUnauthenticated, (req, res) => {
+
+    // let queryText = `SELECT * FROM "teams" WHERE `;
+    // pool.query(queryText).then((result) => {
+    //     console.log(result.rows);
+            // axios request to the sportradar.us API
+            axios({
+                method: `GET`,
+                url: `${BASE_URL}tournaments/sr:tournament:17/info.json?api_key=${API_KEY}`
+            }).then((response) => {
+                res.send(response.data);
+            }).catch((axiosError) => {
+                // console log and client error message for axios request
+                console.log(`Error in axios GET request for team data: ${error}`);
+                res.sendStatus(500);
+            }); 
+    // }).catch((poolError) => {
+    //     // console log and client message for error
+    //     console.log(`Error in API get for initial team stats: ${poolError}`);
+    //     res.sendStatus(500);
+    // });
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
