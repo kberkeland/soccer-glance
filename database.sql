d053ea7ec2869d8b8f21ea7382538bb5786946b8
Kye Berkeland <kyeberkeland@yahoo.com>
2019-02-15 09:32:33 -0600

    Add to sql

--- database.sql
+++ database.sql
@@ -3,3 +3,47 @@ CREATE TABLE "person" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "person" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL
+);
+
+DROP TABLE "teams";
+
+CREATE TABLE "my_teams" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80),
+	"competitor_id" VARCHAR(80),
+	"league_id" INT REFERENCES "leagues"
+);
+
+DROP TABLE "tournament";
+
+CREATE TABLE "leagues" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80),
+	"tournament_id" VARCHAR(80)
+);
+
+INSERT INTO "leagues" ("name", "tournament_id")
+VALUES ( 'Spanish La Liga', 'sr:tournament:8' ),
+( 'English Premier League', 'sr:tournament:17' ),
+( 'Italian Serie A', 'sr:tournament:23' ),
+( 'French Ligue 1', 'sr:tournament:34' ),
+( 'German Bundesliga', 'sr:tournament:35' ),
+( 'Dutch Eredivisie', 'sr:tournament:37' ),
+( 'Belgium First Division A', 'sr:tournament:38' ),
+( 'Turkish Super Lig', 'sr:tournament:52' ),
+( 'Greek Super League', 'sr:tournament:185' ),
+( 'Russian Premier League', 'sr:tournament:203' ),
+( 'Ukrainian Premier League', 'sr:tournament:218' ),
+( 'Portuguese Premier League', 'sr:tournament:238' );
+
+INSERT INTO "my_teams" ("name", "competitor_id", "league_id")
+VALUES ( 'Manchester City', 'sr:competitor:17', '3' ),
+( 'Manchester United', 'sr:competitor:35', '3' ),
+( 'Chelsea FC', 'sr:competitor:38', '3' ),
+( 'Wolverhampton Wanderers', 'sr:competitor:3', '3' ),
+( 'Arsenal FC', 'sr:competitor:42', '3' );
--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -6,7 +6,7 @@ class InfoPage extends Component {
   componentDidMount() {
     // use component did mount to dispatch an action to request a list of leagues and teams
     // this.props.dispatch({type: 'FETCH_LEAGUES'});
-    this.props.dispatch({type: 'FETCH_TEAM_STATS'});
+    this.props.dispatch({type: 'FETCH_TEAMS'});
   }
 
   handleClick = () => {
@@ -17,17 +17,17 @@ class InfoPage extends Component {
   render() {
     return (
       <div>
-        {JSON.stringify(this.props.reduxStore.teams.teamStatsReducer)}
+        {JSON.stringify(this.props.reduxStore.teams.teamsReducer)}
         {/* <ul>
         {this.props.reduxStore.leagues.map((leagues, i) => (
           <li key={i}>{leagues.id} {leagues.name} <button onClick={this.handleClick}>Select League</button></li>
         ))}
         </ul> */}
-        {/* <ul>
-        {this.props.reduxStore.teams.map((teams, i) => (
+        <ul>
+        {this.props.reduxStore.teams.teamsReducer.map((teams, i) => (
                             <li key={i}>{teams.id}{teams.name}</li>
         ))}
-        </ul> */}
+        </ul>
       </div>
     )
   }
